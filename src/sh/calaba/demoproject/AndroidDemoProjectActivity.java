ea02249df5409e8ec9b8fad6ad49135df319af4b
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-05-21 19:50:36 -0300

    Initial commit

---
+++ src/sh/calaba/demoproject/AndroidDemoProjectActivity.java
@@ -0,0 +1,13 @@
+package sh.calaba.demoproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidDemoProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
